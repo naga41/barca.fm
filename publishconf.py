2e6dbbb713aee0870fb68e7382189f7b34b4197d
Shuhei Nagata <shnagata.41@gmail.com>
2019-03-25 23:28:29 +0900

    Set up publishconf

--- publishconf.py
+++ publishconf.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
-# from pelicanconf import *
 
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
@@ -9,13 +8,14 @@ from __future__ import unicode_literals
 import os
 import sys
 sys.path.append(os.curdir)
+from pelicanconf import *  # noqa: E402, F401, F403
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'barca.fm'
+SITEURL = 'https://www.barca.fm'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
